e182ccfdef7c42e08c15f622308f9798a0010b21
Ai Chang <ac@AideMacBook-Air.local>
2025-10-07 16:24:18 +0800

    clear all header files and source files so that I can do it again
    clear server.h, client.h, server.cpp, client.cpp

--- AP1400-2-HW2/include/server.h
+++ AP1400-2-HW2/include/server.h
@@ -1,32 +1,4 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <string>
-#include <map>
-#include <memory>
-
-class Client;
-
-// global variable should be defined in the source file
-// define this outside the Server class and save the pending transactions in it
-extern std::vector<std::string> pending_trxs;
-
-class Server
-{
-public:
-	Server();
-	std::shared_ptr<Client> add_client(std::string id);
-	std::shared_ptr<Client> get_client(std::string id) const;
-	double get_wallet(std::string id) const;
-  // trx: sender-receiver-value
-  // if the string is not standard, throw a runtime error
-	static bool parse_trx(const std::string& trx, std::string& sender, std::string& receiver, double& value);
-	bool add_pending_trx(std::string trx, std::string signature) const;
-	size_t mine();
-private:
-	std::map<std::shared_ptr<Client>, double> clients;
-};
-
-void show_wallets(const Server& server);
-
 #endif //SERVER_H
