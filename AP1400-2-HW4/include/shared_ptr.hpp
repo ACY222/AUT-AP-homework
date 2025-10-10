4f476e143347d18039aaed7420e2644bb1220e93
Ai Chang <ac@AideMacBook-Air.local>
2025-10-10 10:19:33 +0800

    my first try for hw4

---
+++ AP1400-2-HW4/include/shared_ptr.hpp
@@ -0,0 +1,94 @@
+#ifndef SHARED_PTR_HPP
+#define SHARED_PTR_HPP
+
+#include <cstddef>
+#include <utility>
+template<typename T>
+class SharedPtr {
+private:
+  T* _p = nullptr;
+  size_t* count_p = nullptr;
+
+public:
+  // some functions that operator the count
+  void increment() {
+    ++(*count_p);
+  }
+  void decrement() {
+    --(*count_p);
+  }
+  bool zero() {
+    return *count_p == 0;
+  }
+  // constructor
+  SharedPtr() : _p(nullptr), count_p(nullptr) {}
+  SharedPtr(T* p) : _p(p), count_p(nullptr) {
+    if (_p) {
+      count_p = new size_t(1);  // initialize the count with 1
+    }
+  }
+  // destructor
+  ~SharedPtr() {
+    if (this->count_p) {
+      this->decrement();
+      if (this->zero()) {
+        delete _p;
+      }
+    }
+    else {
+      delete _p;
+    }
+    _p = nullptr;
+  }
+  // copy constructor and copy assignment operator
+  SharedPtr(const SharedPtr& other) :
+    _p(other->_p),
+    count_p(other.count_p) {
+    this->increment();
+  }
+
+  SharedPtr& operator=(const SharedPtr& other) {
+    this->_p = other._p;
+    this->count_p = other.count_p;
+    this->increment();
+    return *this;
+  }
+
+  size_t use_count() {
+    return *this->count_p;
+  }
+
+  T* get() {
+    return this->_p;
+  }
+
+  T operator*() {
+    return *this->_p;
+  }
+
+  T* operator->() {
+    return this->_p;
+  }
+
+  void reset() {
+    delete _p;
+    _p = nullptr;
+    delete count_p;
+    count_p = nullptr;
+  }
+
+  void reset(T* ptr) {
+    delete _p;
+    _p = ptr;
+    delete count_p;
+    count_p = new size_t(1);
+  }
+};
+
+template<typename T, typename... Args>
+SharedPtr<T> make_shared(Args&&... args) {
+  return SharedPtr<T>(new T(std::forward<Args>(args)...));
+}
+
+
+#endif
