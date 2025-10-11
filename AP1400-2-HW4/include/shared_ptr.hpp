b11d8cc9cc2ff8399aabcd551667e6d0f7cb5754
Ai Chang <ac@AideMacBook-Air.local>
2025-10-11 09:29:50 +0800

    Revert "clear two header files in hw4, so that I can try it again"
    This reverts commit 99da2c099170e5ce53621b36248fbb6bacc2dd4a.

--- AP1400-2-HW4/include/shared_ptr.hpp
+++ AP1400-2-HW4/include/shared_ptr.hpp
@@ -1,4 +1,117 @@
 #ifndef SHARED_PTR_HPP
 #define SHARED_PTR_HPP
 
+#include <cstddef>
+#include <utility>
+template<typename T>
+class SharedPtr {
+private:
+  T* _p = nullptr;
+  size_t* count_p = nullptr;
+
+  // some functions that operator the count
+  void increment() {
+    ++(*count_p);
+  }
+  void decrement() {
+    --(*count_p);
+    if (this->zero()) { // delete them when count is decreased to 0
+      delete this->_p;
+      delete this->count_p;
+      this->_p = nullptr; // because count == 0, now there's no other pointers
+      this->count_p = nullptr;
+    }
+  }
+  bool zero() {
+    if (count_p)
+      return *count_p == 0;
+    return false;
+  }
+
+public:
+  // constructor
+  SharedPtr() : _p(nullptr), count_p(nullptr) {}
+  SharedPtr(std:: nullptr_t) : _p(nullptr), count_p(nullptr) {}
+  SharedPtr(T* p) : _p(p), count_p(nullptr) {
+    if (_p) {
+      count_p = new size_t(1);  // initialize the count with 1
+    }
+  }
+  // destructor
+  ~SharedPtr() {
+    if (this->count_p) {
+      // decrease the count, if it reaches 0, then delete automatically
+      this->decrement();
+    }
+  }
+  // copy constructor and copy assignment operator
+  SharedPtr(const SharedPtr& other) :
+    _p(other._p),
+    count_p(other.count_p) {
+    this->increment();
+  }
+
+  SharedPtr& operator=(const SharedPtr& other) {
+    SharedPtr temp(other);
+    // if this == &other, when going out of this function, temp calls
+    // destructor, count--, so we won't be affected
+    swap(*this, temp);
+    return *this;
+  }
+
+  size_t use_count() {
+    return count_p ? *count_p : 0;
+  }
+
+  T* get() const {
+    return this->_p;
+  }
+
+  T& operator*() {
+    return *this->_p;
+  }
+
+  const T& operator*() const {
+    return *this->_p;
+  }
+
+  T* operator->() {
+    return this->_p;
+  }
+
+  const T* operator->() const {
+    return this->_p;
+  }
+
+  void reset() {
+    if (!_p) return;    // if _p == nullptr, do nothing
+    this->decrement();
+
+    _p = nullptr;
+    count_p = nullptr;
+  }
+
+  void reset(T* ptr) {
+    if (this->_p == ptr) return;
+    SharedPtr temp(ptr);
+    swap(*this, temp);
+  }
+
+  explicit operator bool() {
+    return _p != nullptr;
+  }
+
+  friend void swap(SharedPtr& a, SharedPtr& b) noexcept {
+    using std::swap;
+    swap(a._p, b._p);
+    swap(a.count_p, b.count_p);
+  }
+};
+
+template<typename T, typename... Args>
+SharedPtr<T> make_shared(Args&&... args) {
+  return SharedPtr<T>(new T(std::forward<Args>(args)...));
+}
+
+
 #endif
--- AP1400-2-HW4/include/unique_ptr.hpp
+++ AP1400-2-HW4/include/unique_ptr.hpp
@@ -1,4 +1,73 @@
 #ifndef UNIQUE_PTR_HPP
 #define UNIQUE_PTR_HPP
 
+#include <utility>
+template<typename T>
+class UniquePtr {
+private:
+  T* _p = nullptr;    // the ptr
+  //
+public:
+  // default constructor
+  UniquePtr() : _p(nullptr) {}
+  // constructor
+  UniquePtr(T* p) : _p(p) {}
+  // destructor
+  ~UniquePtr() {
+    if (_p) {   // avoid delete twice
+      delete _p;
+      _p = nullptr;
+    }
+  }
+  // delete copy constructor and copy assignment operator
+  UniquePtr(const UniquePtr& other) = delete;
+  UniquePtr& operator=(const UniquePtr& other) = delete;
+  // get, return the raw pointer stored in the class
+  T* get() const {
+    return _p;
+  }
+  // operator*, dereference
+  T& operator*() {
+    return *_p;
+  }
+  const T& operator*() const {
+    return *_p;
+  }
+  // operator->, arrow operator like normal pointers
+  T* operator->() {
+    return _p;
+  }
+  const T* operator->() const {
+    return _p;
+  }
+  // reset, delete the pointer and assign `nullptr` to it
+  void reset() {
+    delete _p;
+    _p = nullptr;
+  }
+  // reset, have an input, make a new pointer with it after deleting the old
+  void reset(T* p) {
+    delete _p;
+    _p = p;
+  }
+  // release, returns the stored pointer in the class(like get), with 2
+  // differences: 1. the UniquePtr class won't store the pointer anymore, 2.
+  //    deleting the pointer should not be done by UniquePtr class after calling
+  //    release()
+  T* release() {
+    T* temp = _p; // now they point to the same space
+    _p = nullptr;
+    return temp;
+  }
+
+  explicit operator bool() {
+    return _p != nullptr;
+  }
+};
+
+template<typename T, typename... Args>
+UniquePtr<T> make_unique(Args&&... args) { // learn about this function
+  return UniquePtr<T>(new T(std::forward<Args>(args)...));
+}
+
 #endif
